c5e052a7e073c0c7681a3a69c22eb27e4ec1db92
LauBetancur <Lau1betanur1@gmail.com>
2024-03-07 08:18:11 -0500

    todo

---
+++ script.js
@@ -0,0 +1,14 @@
+const num1 =parseInt(prompt("ingrese el primer numero"));
+const num2 =parseInt(prompt("ingrese el segundo numero"));
+const num3 =parseInt(prompt("ingrese el tercer numero"));
+const parrafo =document.querySelector("#parrafo");
+
+if (num1 > num2 && num1>num3) { 
+    parrafo.innerHTML ="el numero mas grande es" + num1;
+}else if(num2>num1&& num2>num3){
+    parrafo.innerHTML ="el numero mas grande es" + num2;
+}else{
+    parrafo.innerHTML ="el numero mas grande es" + num3;
+}
+
+
